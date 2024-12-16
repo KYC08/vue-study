df5e60ce86fbb4cbe24090d281627b9815fdbe3c
youngchan <yckim8721@gmail.com>
2024-12-16 12:21:21 +0900

    #1 notice search 구현 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login/Login.vue';
 import DashBoard from '../views/DashBoard/DashBoard.vue';
+import Notice from '../views/Board/Notice.vue';
 
 const routes = [
-    {
-        path: '/',
-        name: 'login',
-        component: Login,
-    },
-    {
-        path: '/vue',
-        name: 'vue',
-        component: DashBoard,
-    },
+  {
+    path: '/',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/vue',
+    name: 'vue',
+    component: DashBoard,
+    children: [
+      {
+        path: 'board',
+        name: 'board',
+        children: [
+          {
+            // notice.do -> localhost:5173/board/notice.do
+            // /notice.do -> localhost:5173/notice.do
+            path: 'notice.do',
+            name: 'notice',
+            component: Notice,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory('/'),
-    routes,
+  history: createWebHistory('/'),
+  routes,
 });
 
 export default router;
